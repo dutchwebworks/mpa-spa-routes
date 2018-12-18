3a1ff1ac8a3a433cdedbba5947338dd2c4e01d63
Dennis Burger <dennis.burger@poort80.nl>
2018-12-18 14:05:10 +0100

    Updated demo links

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
-const routes = [
-    {
-        path: "/foo",
-        component: Foo
-    },
-    {
-        path: "/bar",
-        component: Bar
-    },
-];
+const Step1 = { template: '<div>Step 1</div>' }
+const Step2 = { template: '<div>Step 2</div>' }
+const Step3 = { template: '<div>Step 3</div>' }
 
 const router = new VueRouter({
-    routes
+    mode: "history",
+    base: "/orderflow",
+    routes : [
+        {
+            path: "/step-1",
+            component: Step1
+        },
+        {
+            path: "/step-2",
+            component: Step2
+        },
+        {
+            path: "/step-3",
+            component: Step3
+        }
+    ]
 });
 
 const app = new Vue({
